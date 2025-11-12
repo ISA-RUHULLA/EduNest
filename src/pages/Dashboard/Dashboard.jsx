e2583a2d0c08f559620fa144a9d795fb10bc2d62
unknown <isaruhullah035@gmail.com>
2025-11-12 23:15:39 +0600

    About section added

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold mb-6 text-center">Dashboard</h2>
+            <h1 className='text-4xl font-bold text-center'>Dashboard Feature are Coming Soon...</h1>
+            <p className='text-xl font-bold text-center'>You can Explore others Option</p>
+            <div className='flex justify-center mt-auto pt-4 gap-2 w-full '>
+                <Link to="/" className='btn btn-primary'>Back to Home</Link>
+            </div>
         </div>
     );
 };
